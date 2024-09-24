43ba3235b0ca7b044c8e17b3c544776ec49d8ec9
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-24 13:16:03 +0000

    comments

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from tkinter import Tk, Label, Button, Canvas, PhotoImage
 def main()->None:
+    #Create a window 
     window = Tk()
+    # Title of the app
+    window.title("Password Generator")
     canvas = Canvas(window, width=200, height=200, bg="green",)
-    
+    #Load logo
     logo = PhotoImage(file="logo.png")
+    #Create image on the canvas
     canvas.create_image(100, 100, image=logo)
-    
     canvas.grid(padx=20, pady=20, )
 
-    window.title("Password Generator")
-
-    
+    # Start main loop
     window.mainloop()
-    
+
 if __name__ == "__main__": 
     main()
