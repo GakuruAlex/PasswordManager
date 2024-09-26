98bf0d42bfb70785054c6947236c2901c4aeaa29
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-26 13:18:56 +0300

    insert generated password into the password field

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 import string
 from random import choice, randint, shuffle
 class Password:
-    def __init__(self):
+    def __init__(self, password_field):
         self.letters = string.ascii_letters
         self.numbers  = string.digits
         self.symbols = ["@", "!", "$", "#", "&", "*", "+",]
         self.num_letters = randint(8, 10)
         self.num_numbers = randint(2, 4)
         self.num_symbols = randint(2, 4)
+        self.password = ""
+        self.password_field = password_field
 
     def generate_password(self)-> str:
         """_Generate a password that includes letters, numbers and symbols_
@@ -15,9 +17,12 @@ class Password:
         Returns:
             str: _generated password_
         """
-        password = [choice(self.letters) for _ in range(self.num_letters)]
-        password += [choice(self.numbers) for _ in range(self.num_numbers)]
-        password += [choice(self.symbols) for _ in range(self.num_symbols)]
+        self.password_field.delete(0, 'end')
+        password_1 = [choice(self.letters) for _ in range(self.num_letters)]
+        password_1 += [choice(self.numbers) for _ in range(self.num_numbers)]
+        password_1 += [choice(self.symbols) for _ in range(self.num_symbols)]
 
-        shuffle(password)
-        return "".join(password)
+        shuffle(password_1)
+        self.password= "".join(password_1)
+        self.password_field.insert(0, self.password)
+
