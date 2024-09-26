0696b40a5e57171f98c0f2dc103c6e560c56c69c
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-26 11:05:05 +0300

    check whether password or website is empty and show a dialog if any is empty

--- save_data.py
+++ save_data.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 class SaveData:
     def __init__(self, website, email, password):
         self.website = website
@@ -15,9 +16,22 @@ class SaveData:
     def save_data_file(self)->None:
         """_Write the website, email and password to data text file_
         """
-        with open("data.txt", "a") as file:
-                file.write(f"Website: {self.data['website']} | Email: {self.data['email']} | Password: {self.get_data()['password']}\n")
-        self.clear_data()
-    def clear_data(self):
+        self.get_data()
+        if self.is_website_or_password_empty():
+            messagebox.showerror(title="Missing Fields", message="Website and Password cannot be empty")
+        else:
+            with open("data.txt", "a") as file:
+                file.write(f"Website: {self.data['website']} | Email: {self.data['email']} | Password: {self.data['password']}\n")
+                self.clear_data()
+    def clear_data(self)->None:
+        """_Clear the content of the input fields_
+        """
         self.website.delete(0, 'end')
         self.password.delete(0, 'end')
+    def is_website_or_password_empty(self)-> bool:
+        """_Check whether password and website are empty_
+
+        Returns:
+            bool: _True if website or password is empty otherwise False_
+        """
+        return len(self.data['website'])==0 or len(self.data['password']) == 0
